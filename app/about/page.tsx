5673b6f7908392c736f7622590a11eb681cfd067
Victor M. Lopez Jr <vlopezjr@hotmail.com>
2023-09-14 16:01:46 -0700

    Lesson 2

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function About() {
+
+  return (
+    <>
+        <div>About Page</div>
+        <Link href="/">Link to Home Page</Link>
+    </>
+    
+  )
+}
